159b3da14a7b7fe88a6ae6df30cda2d6d7afa406
Mike Knoop <mikeknoop@gmail.com>
2012-02-21 20:37:06 -0600

    adding 3d gplot function, adding plotting for deformed and undeformed shape in output

--- fea_output.m
+++ fea_output.m
@@ -22,15 +22,39 @@ elname = D.elname;  % name of element Mfile
 % initialize equation numbers for dofs
 ieqn = initialize(D);
 
-stresses = zeros(nel, 1)
 %
-% Calculate stresses
+% calculate stresses
+stresses = zeros(nel, 1);
 for i = 1:nel;
     % element stresses
     icode = 3;
     [eldat,ieleqn] = feval(elname,eprops(iprops(i),:),ielem(i,1:2),ieqn,D,icode);
-    stresses(i) = eldat
+    stresses(i) = eldat;
 end
+
+%
+% plot 2D undeformed and deformed elements to screen
+sf = 1e7; % factor to scale deformations by
+xy = x;
+xy(:,3) = []; % delete z data column
+A = zeros(nnode,nnode)
+for i = 1:nel;
+    m = ielem(i,1);
+    n = ielem(i,2);
+    A(m,n) = 1;
+end;
+gplot3(A,xy) % plot undeformed
+axis([min(xy(:,1))-2 max(xy(:,1))+2 min(xy(:,2))-2 max(xy(:,2))+2]) % zoom out
+hold on;
+% add on deformations
+for i = 1:nnode;
+    xy(i,1) = xy(i,1)+sf*(utot((2*i)-1));
+    xy(i,2) = xy(i,2)+sf*(utot((2*i)));
+end;
+gplot3(A,xy,'red') % plot deformed
+axis([min(xy(:,1))-2 max(xy(:,1))+2 min(xy(:,2))-2 max(xy(:,2))+2]) % zoom out
+legend('undeformed', ['deformed (scaled ' num2str(sf,'%G') ')'])
+hold off;
 
 %
 % print the following to the screen
